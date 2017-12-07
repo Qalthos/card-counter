c3a4daf927a1e89e0c6222f4f3a996af67f8af25
Nathaniel Case <Qalthos@gmail.com>
2017-12-06 19:14:47 -0500

    Model-based queries are really useful, it turns out

--- card_counter/database.py
+++ card_counter/database.py
@@ -3,9 +3,11 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 engine = create_engine('sqlite:///cards.db', convert_unicode=True)
-session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+DBSession = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+class _Base(object):
+    query = DBSession.query_property()
 
-Base = declarative_base()
+Base = declarative_base(cls=_Base)
 
 def init_db():
     import card_counter.models
