95933ff7fa5f1734d33f30e8ada0969983df5ca9
Nathaniel Case <Qalthos@gmail.com>
2017-12-08 15:11:47 -0500

    Woo lambdas

--- card_counter/site.py
+++ card_counter/site.py
@@ -3,7 +3,7 @@ import random
 
 from flask import Flask, render_template
 
-from card_counter import stats
+from card_counter.stats import stat_functions
 from card_counter.models import Game, Player
 
 app = Flask(__name__)
@@ -12,13 +12,7 @@ app = Flask(__name__)
 def overview():
     player_count = Player.query.count()
     game_count = Game.query.count()
-    rendered_stats = random.sample((
-        stats.lowest_scores(),
-        stats.highest_scores(),
-        stats.winningest_players(),
-        stats.best_players(),
-        stats.recent_games(),
-    ), 2)
+    rendered_stats = [function() for function in random.sample(stat_functions, 2)]
     return render_template('index.html', player_count=player_count, game_count=game_count, stats=rendered_stats)
 
 
--- card_counter/stats.py
+++ card_counter/stats.py
@@ -3,6 +3,8 @@ from flask import render_template_string
 from card_counter.models import Game, Player, Score
 
 
+WIN_TEMPLATE = '{{{{ item|safe }}}} has won {0} games'
+
 def _render(title, data):
     template = '''
 <h2>{{ title }}</h2>
@@ -12,32 +14,18 @@ def _render(title, data):
 '''
     return render_template_string(template, title=title.format(len(data)), data=data)
 
-def lowest_scores():
-    scores = Score.query.order_by(Score.score).limit(5).all()
-    return _render('{} lowest scores', scores)
+
+def _custom_render(title, item_template, iterable):
+    data = [render_template_string(item_template, item=item) for item in iterable][:5]
+    return _render(title, data)
 
 
-def highest_scores():
-    scores = Score.query.order_by(Score.score.desc()).limit(5).all()
-    return _render('{} highest scores', scores)
-
-
-def winningest_players():
-    template = "{{ player|safe }} has won {{ player.won_games|length }} games"
-    players = sorted(Player.query.all(), key=lambda p:len(p.won_games), reverse=True)[:5]
-    player_strings = [render_template_string(template, player=player) for player in players]
-
-    return _render('{} winningest players', player_strings)
-
-
-def best_players():
-    template = "{{ player|safe }} has won {{ (100 * player.won_games|length) // player.games|length }}% of games"
-    players = sorted(Player.query.all(), key=lambda p:len(p.won_games)/len(p.games), reverse=True)[:5]
-    player_strings = [render_template_string(template, player=player) for player in players]
-
-    return _render('{} top players', player_strings)
-
-
-def recent_games():
-    games = Game.query.order_by(Game.date.desc()).limit(5).all()
-    return _render('{} most recent games', games)
+stat_functions = [
+    lambda : _render('{} lowest scores', Score.query.order_by(Score.score).limit(5).all()),
+    lambda : _render('{} highest scores', Score.query.order_by(Score.score.desc()).limit(5).all()),
+    lambda : _custom_render('{} winningest players', WIN_TEMPLATE.format("{{ item.won_games|length }}"),
+                            sorted(Player.query.all(), key=lambda p:len(p.won_games), reverse=True)),
+    lambda : _custom_render('{} top players', WIN_TEMPLATE.format("{{ 100 * item.won_games|length // item.games|length }}% of"),
+                            sorted(Player.query.all(), key=lambda p:len(p.won_games)/len(p.games), reverse=True)),
+    lambda : _render('{} most recent games', Game.query.order_by(Game.date.desc()).limit(5).all()),
+]
